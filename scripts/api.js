f36ad8ff42c4ca1eab9f866c0188f4fe78b66b43
Martin Chaine <martin@casimir.me>
2016-11-16 00:13:37 +0100

    use Vue methods instead of globals

--- scripts/api.js
+++ scripts/api.js
@@ -2,50 +2,53 @@
 
 function api(uri, cb) {
     var url = "https://overseer.casimir-lab.net/" + uri;
-    var method = 'GET';
     var req = new XMLHttpRequest();
-    req.onerror = function() { console.log("Request failed"); };
+    req.open('GET', url);
     req.onload = function () {
         var data = JSON.parse(req.responseText);
         cb(data);
     };
-    req.open('GET', url);
     req.send();
 }
 
-function updateContent() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var latitude = parseFloat(position.coords.latitude);
-        var longitude = parseFloat(position.coords.longitude);
-        api("now/@" + latitude + "," + longitude, function(data) {
-            var station = data.bike.station
-            var distance = parseInt(data.bike.distance) + "m";
-            app.now.message = station.name + " (" + distance + ") : 🚲 " + station.bikes + ", 🅿 " + station.slots
-        });
-    }, function showError(error) {
-	switch(error.code) {
-		case error.PERMISSION_DENIED:
-			alert("User denied the request for Geolocation.");
-			break;
-		case error.POSITION_UNAVAILABLE:
-			alert("Location information is unavailable.");
-			break;
-		case error.TIMEOUT:
-			alert("The request to get user location timed out.");
-			break;
-		case error.UNKNOWN_ERROR:
-			alert("An unknown error occurred.");
-			break;
-	}
-    }, {enableHighAccuracy: true});
-};
-
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
         now: {
             message: "Récupération de la position GPS...",
         },
     },
-    ready: updateContent(),
+    created: function() {
+        this.fetchNowData();
+    },
+    methods: {
+        fetchNowData: function() {
+            var self = this;
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var latitude = parseFloat(position.coords.latitude);
+                var longitude = parseFloat(position.coords.longitude);
+                api("now/@" + latitude + "," + longitude, function(data) {
+                    var station = data.bike.station;
+                    var distance = parseInt(data.bike.distance) + "m";
+                    self.now = data;
+                    self.now.message = station.name + " (" + distance + ") : 🚲 " + station.bikes + ", 🅿 " + station.slots;
+                });
+            }, function showError(error) {
+        	switch(error.code) {
+        		case error.PERMISSION_DENIED:
+        			alert("User denied the request for Geolocation.");
+        			break;
+        		case error.POSITION_UNAVAILABLE:
+        			alert("Location information is unavailable.");
+        			break;
+        		case error.TIMEOUT:
+        			alert("The request to get user location timed out.");
+        			break;
+        		case error.UNKNOWN_ERROR:
+        			alert("An unknown error occurred.");
+        			break;
+        	}
+            }, {enableHighAccuracy: true});
+        }
+    }
 });
